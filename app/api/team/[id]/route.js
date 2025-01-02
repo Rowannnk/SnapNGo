6d449de538a8b0523a3d8bfa88387e1f23f18bf6
rowan <u6511498@au.edu>
2025-01-02 15:28:48 +0700

    team delete route

--- app/api/team/[id]/route.js
+++ app/api/team/[id]/route.js
@@ -1,5 +1,6 @@
 import Team from "@/models/Team";
 import dbConnect from "@/utils/dbConnect";
+import User from "@/models/User";
 import { NextResponse } from "next/server";
 
 export async function GET(request, { params }) {
@@ -25,6 +26,48 @@ export async function GET(request, { params }) {
     console.error("Error retrieving team:", error);
     return NextResponse.json(
       { message: "An error occurred while retrieving the team." },
+      { status: 500 }
+    );
+  }
+}
+
+export async function DELETE(request, { params }) {
+  try {
+    const { id } = params;
+    const { adminUsername } = await request.json();
+
+    await dbConnect();
+
+    const team = await Team.findById(id);
+
+    if (!team) {
+      return NextResponse.json({ message: "Team not found." }, { status: 404 });
+    }
+
+    // Check if the adminUsername matches
+    if (team.adminUsername !== adminUsername) {
+      return NextResponse.json(
+        { message: "Only the admin who created the team can delete it." },
+        { status: 403 }
+      );
+    }
+
+    // Remove the team ID from all users' teamIds array
+    await User.updateMany(
+      { teamIds: id }, // Find users where the team ID exists
+      { $pull: { teamIds: id } } // Remove the team ID from the teamIds array
+    );
+
+    await Team.findByIdAndDelete(id);
+
+    return NextResponse.json(
+      { message: "Team deleted successfully." },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error deleting team:", error);
+    return NextResponse.json(
+      { message: "An error occurred while deleting the team." },
       { status: 500 }
     );
   }
